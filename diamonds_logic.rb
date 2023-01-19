b3ae5c7de470ec415ee33647344f5518aa32553e
gabriella garcia <gabirvargas@gmail.com>
2023-01-19 15:36:24 -0300

    code without the debugging prints with a more readable result

--- diamonds_logic.rb
+++ diamonds_logic.rb
@@ -1,22 +1,19 @@
 # TODO extrair os "diamantes" <> e as "areias" . da expressão,
 # TODO exibir a quantidade de diamantes extraídos.
 
-sd = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
+sand_and_diamonds = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
 #1 criar um local para salvar os diamantes encontrados
 diamonds = []
 
-#2 extrair as areias e salvar para utilizar o restante da expressão
-without_sand = sd.gsub(".", "")
+#2 extrair as areias e salvar a nova string para utilizar o restante da expressão
+without_sand = sand_and_diamonds.gsub(".", "")
 
 #3 criar um loop para extrair os diamantes enquanto existirem
 while without_sand.include?"<>"
   diamond_out = without_sand.slice!("<>")
-  p without_sand
-  p diamond_out
-  #4 salvar os diamantes encontrados
+  #4 salvar o diamante encontrado
   diamonds << diamond_out
-  p diamonds
 end
 
 #5 contar os diamantes encontrados
-p diamonds.count
+puts "Foram encontrados #{diamonds.count} diamantes"
